33431b0f63c0e98fc66742f034179d9bfc0564b7
Hermansyah <herman.piliang23@gmail.com>
2022-11-03 18:17:48 +0700

    update: index schema

--- src/Domains/IndexSchema.js
+++ src/Domains/IndexSchema.js
@@ -1,5 +1,5 @@
 export default class IndexSchema {
-  constructor(page = 1, count = 15, search = []) {
+  constructor({page = 1, count = 15, search = []}) {
     this.page = page;
     this.count = count;
     this.search = search; // TODO: filter search not implemented yet
--- src/Domains/_test/IndexSchema.test.js
+++ src/Domains/_test/IndexSchema.test.js
@@ -3,7 +3,7 @@ import IndexSchema from '../IndexSchema';
 describe('IndexSchema', () => {
   it('should called correctly', () => {
     // Arrange
-    const indexSchema = new IndexSchema();
+    const indexSchema = new IndexSchema({});
     // Action and Assert
     expect(indexSchema.page).toEqual(1);
     expect(indexSchema.count).toEqual(15);
