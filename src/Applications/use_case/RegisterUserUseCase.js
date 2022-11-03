2f3447b90317b5b85115fd9513414d7ebd122400
Hermansyah <herman.piliang23@gmail.com>
2022-11-03 08:49:47 +0700

    add: register use use case

---
+++ src/Applications/use_case/RegisterUserUseCase.js
@@ -0,0 +1,47 @@
+import UserRepository from '../../Domains/users/UserRepository';
+import TokenManager from '../security/TokenManager';
+import TokenRepository from '../../Domains/authentications/TokenRepository';
+import AuthToken from '../../Domains/authentications/schemas/AuthToken';
+import GeneratedToken from '../../Domains/authentications/schemas/GeneratedToken';
+import dayjs from 'dayjs';
+import { REFRESH_TOKEN_EXPIRE } from '../../Commons/Constants/common';
+import RegisterUser from '../../Domains/users/schemas/RegisterUser';
+import RegisteredUser from '../../Domains/users/schemas/RegisteredUser';
+
+export default class RegisterUserUseCase {
+  /**
+   *
+   * @param {TokenManager} tokenManager
+   * @param {TokenRepository} tokenRepository
+   * @param {UserRepository} userRepository
+   */
+  constructor({ tokenManager, tokenRepository, userRepository, passwordHash }) {
+    this._tokenManager = tokenManager;
+    this._tokenRepository = tokenRepository;
+    this._userRepository = userRepository;
+  }
+
+  async execute(payload){
+    const userPayload = new RegisterUser(payload);
+    /**@type {RegisteredUser} */
+    const registeredUser = await this._userRepository.store(userPayload);
+
+    const tokenPayload = { username: registeredUser.username }; // refreshToken will be mutated ang get expired seconds
+    const accessToken = await this._tokenManager.createAccessToken(tokenPayload);
+    const refreshToken = await this._tokenManager.createRefreshToken(tokenPayload);
+
+    const expiredAt = tokenPayload.exp 
+      ? dayjs(tokenPayload.exp * 1000).format('YYYY-MM-DD HH:mm:ss') 
+      : dayjs().add(REFRESH_TOKEN_EXPIRE,'second').format('YYYY-MM-DD HH:mm:ss');
+    const authToken = new AuthToken({accessToken: accessToken, refreshToken: refreshToken});
+    const generatedToken = new GeneratedToken({
+      token: refreshToken, 
+      expired_at: expiredAt
+    });
+    await this._tokenRepository.store(generatedToken);
+    return {
+        user: registeredUser,
+        token: authToken
+    };
+  }
+}
---
+++ src/Applications/use_case/_test/RegisterUserUseCaseTest.test.js
@@ -0,0 +1,63 @@
+import dayjs from "dayjs";
+import AuthToken from "../../../Domains/authentications/schemas/AuthToken";
+import GeneratedToken from "../../../Domains/authentications/schemas/GeneratedToken";
+import TokenRepository from "../../../Domains/authentications/TokenRepository";
+import PasswordUser from "../../../Domains/users/schemas/PasswordUser";
+import UserRepository from "../../../Domains/users/UserRepository";
+import TokenManager from "../../security/TokenManager";
+import RegisterUserUseCase from "../RegisterUserUseCase";
+import RegisterUser from '../../../Domains/users/schemas/RegisterUser';
+import RegisteredUser from '../../../Domains/users/schemas/RegisteredUser';
+
+describe('RegisterUserUseCase', () => { 
+    it('should orchestrating the get authentication action correctly', async () => {
+        // Arrange
+        const useCasePayload = {
+            fullname: 'Hermansyah',
+            username: 'me@hermansyah.dev',
+            email: 'me@hermansyah.dev',
+            password: 'password'
+        };
+        const registerPayload = new RegisterUser(useCasePayload);
+        const registeredUser = new RegisteredUser(registerPayload);
+
+        const expectedAuthentication = new AuthToken({
+            accessToken: 'access_token',
+            refreshToken: 'refresh_token',
+        })
+        const expectedGeneratedToken = new GeneratedToken({
+            token: expectedAuthentication.refreshToken,
+            expired_at: dayjs().format('YYYY-MM-DD HH:mm:ss')
+        })
+        const mockUserRepository = new UserRepository();
+        const mockTokenRepository = new TokenRepository();
+        const mockTokenManger = new TokenManager();
+
+        // Mocking
+        mockUserRepository.store = jest.fn().mockImplementation(() => Promise.resolve(registeredUser));
+        mockTokenManger.createAccessToken = jest.fn().mockImplementation(() => (
+            Promise.resolve(expectedAuthentication.accessToken)
+        ));
+        mockTokenManger.createRefreshToken = jest.fn().mockImplementation(() => (
+            Promise.resolve(expectedAuthentication.refreshToken)
+        ));
+        mockTokenRepository.store = jest.fn().mockImplementation(() => Promise.resolve(expectedGeneratedToken));
+
+        // create use case instance 
+        const authLoginUseCase = new RegisterUserUseCase({
+            tokenManager: mockTokenManger,
+            tokenRepository: mockTokenRepository,
+            userRepository: mockUserRepository,
+        })
+
+        // Action 
+        const actualRegisterUser = await authLoginUseCase.execute(useCasePayload);
+
+        // Assert
+        expect(actualRegisterUser).toEqual({user: registeredUser, token: expectedAuthentication});
+        expect(mockUserRepository.store).toBeCalled();
+        expect(mockTokenManger.createAccessToken).toBeCalledWith({username: useCasePayload.username});
+        expect(mockTokenManger.createRefreshToken).toBeCalledWith({username: useCasePayload.username});
+        expect(mockTokenRepository.store).toBeCalled();
+    })
+ })
