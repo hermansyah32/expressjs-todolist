81768dec318aaeb3f029ad9618241f751dc59b05
Hermansyah <herman.piliang23@gmail.com>
2022-11-26 08:36:29 +0700

    fix: dotenv helper

--- src/Infrastructures/env/env.js
+++ src/Infrastructures/env/env.js
@@ -1,14 +1,17 @@
+import { existsSync } from 'fs';
 import * as dotenv from 'dotenv';
-let envFile = process.cwd() + './.env';
+let envFile = process.cwd() + '/.env';
 
 switch (process.env.NODE_ENV) {
   case 'production':
-    envFile = process.cwd() + './.env.production';
+    envFile = process.cwd() + '/.env.production';
     break;
   default:
     break;
 }
 
-dotenv.config({ path: envFile });
+if (!existsSync(envFile))
+  console.log('Env file is not exists. App will use system environment.');
+else dotenv.config({ path: envFile });
 
 export default dotenv;
